89057c6b80f0d63a7c6f23b486ee5e081ebcb990
Snehil16 <sa5782@nyu.edu>
2024-04-28 23:31:18 +0400

    User Auth completed

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
-app.config.from_object('config.Config')
+app.config["SECRET_KEY"] = os.urandom(16)
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+mongo = PyMongo(app)
 
 @app.route('/')
 def home():
@@ -9,11 +16,47 @@ def home():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        return login_process()
     return render_template('login.html')
+
+def login_process():
+
+    username = request.form['username']
+    password = request.form['password']
+    user = mongo.db.users.find_one({"username": username})
+
+    if user and check_password_hash(user['password'], password):
+        session['username'] = username
+        return redirect(url_for('home'))
+    else:
+        flash('Invalid username or password')
+        return redirect(url_for('login'))
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        print(request.form)
+        username = request.form['username']
+        password = request.form['password']
+        user_exists = mongo.db.users.find_one({"username": username})
+        if user_exists:
+            flash('Username already exists.')
+            return redirect(url_for('signup'))
+
+        hashed_password = generate_password_hash(password)
+        mongo.db.users.insert_one({
+            "username": username,
+            "password": hashed_password
+        })
+        flash('User created successfully. Please login.')
+        return redirect(url_for('login'))
     return render_template('signup_new.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
 
 @app.route('/addperson', methods=['GET', 'POST'])
 def addperson():
@@ -36,4 +79,5 @@ def update_expense():
     return render_template('update_expense.html')
 
 if __name__ == '__main__':
+    # app.secret_key = 'your_secret_key_here'
     app.run(debug=True)
--- config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    SECRET_KEY = 'your_secret_key_here'
-    MONGO_URI = 'your_mongodb_uri_here'
